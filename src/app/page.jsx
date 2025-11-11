e37a25026f90465cc5e0f0f452fc8ae5982456de
zehracodes29 <rzehra942@gmail.com>
2025-11-11 23:32:22 +0530

    ui enhanced

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,23 @@
+"use client";          
+
 import React from 'react'
 
 const Home = () => {
+      
+
   return (
-    <div>Home</div>
+    
+
+    <div className="home" style={{ backgroundColor: '#e5f09aff', height: '100vh' }}>
+
+    
+      <h1 className="home-title text-center text-black  text-4xl font-bold">Welcome to the Recipe Recommender</h1>
+      <p className="home-description container  padding-12 mx-auto text-center text-gray-700 mt-8 font-bold">Discover new recipes tailored to your preferences!!!!.</p>
+      <div className="text-center mt-8">
+        
+        <button onClick={() => window.location.href='/FileUpload'} className="home-button  bg-gray-900 text-white py-2 px-4 rounded">Get Started</button>
+      </div>
+      </div>
   )
 }
 
